688be9c48e6fe3a709e9d027d747f8544f053e30
gbk tvm <gbktvm@gmail.com>
2023-11-05 18:11:37 +0530

    First time

---
+++ src/main/java/no/hioa/FrontServiceApplication.java
@@ -0,0 +1,20 @@
+package no.hioa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+ 
+@SpringBootApplication
+public class FrontServiceApplication  extends SpringBootServletInitializer {
+	     
+ 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FrontServiceApplication.class);
+    }
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(FrontServiceApplication.class, args);
+	}
+}
